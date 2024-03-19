aa19efd62b020df25debb3e0374fef526cfce67d
HarshalRane04 <85073947+HarshalRane04@users.noreply.github.com>
2024-03-19 23:43:49 +0530

    Update cachematrix.R
    Edit R File and update code with solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The 2 functions generate a matrix that can be 
+## inversed and then caches its inverse
 
-## Write a short comment describing this function
+## 'makeCacheMatrix' function creates a matrix that
+## can cache its inverse, to provide an imput for the 'cacheSolve' function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' finds the inverse of the matrix 
+## inputed from the above 'makeCacheMatrix' function
+## If the already calculated inverse is the same
+## unchanged matrix, then 'cacheSolve' returns the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
 }
+
+
+## ---------------Checking the program------------------------
+## m <- matrix(rnorm(16),4,4)
+## m1 <- makeCacheMatrix(m)
+## cacheSolve(m1)
+
+## [,1]       [,2]       [,3]       [,4]
+## [1,] -0.1653269  0.2592203  0.6176218 -0.7520955
+## [2,]  0.2828334 -0.1853499  0.4511382  0.2094365
+## [3,]  0.1434840  1.0413868 -0.3550853 -0.3261154
+## [4,]  0.1793583 -0.4252171 -0.4371493 -0.1749830
